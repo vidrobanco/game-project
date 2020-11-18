5a8b55e077de00f46872db146656899e5153745d
vidrobanco <74003100+vidrobanco@users.noreply.github.com>
2020-11-18 15:29:23 +0000

    Inventory and fly shooting (still on progress)
    I added a list for the inventory with the stuff for it, the reference for which fly has which index is at the Inventory.cs file with a comment on top of the list.

---
+++ FrogWars/Assets/Scripts/Player/FlyShooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyShooting : MonoBehaviour
+{
+    public Inventory inv;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            inv.ChangeSelFly(1);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (inv.FliesAvailable())
+            {
+                Shoot();
+                inv.ChangeVal(-1);
+            }
+        }
+    }
+
+    void Shoot()
+    {
+
+    }
+}
